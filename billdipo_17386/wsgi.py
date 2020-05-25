260765c5f47e50b7d22338c7a925c3cc21a732fc
Crowdbotics <team@crowdbotics.com>
2020-05-25 21:58:22 +0000

    Initial commit.

---
+++ billdipo_17386/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for billdipo_17386 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'billdipo_17386.settings')
+
+application = get_wsgi_application()
